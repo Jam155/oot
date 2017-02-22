0acdbdae6f061cf6814f20641d5b21062db7ad4f
James Lendrem <James@kinocreative.net>
2017-02-22 13:19:51 +0000

    Event Endpoints return required data

--- includes/Events_Controller.php
+++ includes/Events_Controller.php
@@ -14,25 +14,26 @@
 
 		}
 
-		public function get_offer_meta() {
+		public function get_event_meta() {
 
-			$times_query = "SELECT wp_posts.ID AS post_id, start.start_time, end.end_time, date.date, redeem.redeemable FROM wp_posts
+			$times_query = "SELECT wp_posts.ID AS post_id, start.start_time, end.end_time, date.date, price.price, repeatable.repeatable FROM wp_posts
 					INNER JOIN (SELECT post_id, meta_value AS start_time FROM wp_postmeta WHERE meta_key = 'start_time') AS start ON (wp_posts.ID = start.post_id)
 					INNER JOIN (SELECT post_id, meta_value AS end_time FROM wp_postmeta WHERE meta_key = 'end_time') AS end ON (wp_posts.ID = end.post_id)
 					INNER JOIN (SELECT post_id, meta_value AS date FROM wp_postmeta WHERE meta_key = 'date') AS date ON (wp_posts.ID = date.post_id)
-					INNER JOIN (SELECT post_id, meta_value AS redeemable FROM wp_postmeta WHERE meta_key = 'maximum_redeemable') AS redeem ON (wp_posts.ID = redeem.post_id)";
+					INNER JOIN (SELECT post_id, meta_value AS price FROM wp_postmeta WHERE meta_key = 'ticket_price') AS price ON (wp_posts.ID = price.post_id)
+					INNER JOIN (SELECT post_id, meta_value AS repeatable FROM wp_postmeta WHERE meta_key = 'repeat_event') AS repeatable ON (wp_posts.ID = repeatable.post_id)";
 
 			return $times_query;
 
 		}
 
-		public function get_offers($request) {
+		public function get_events($request) {
 
 			//var_dump($request);
 
-			$offers = array("2 For 1", "50% Off");
+			$events = array("2 For 1", "50% Off");
 			
-			$response  = rest_ensure_response( $offers );
+			$response  = rest_ensure_response( $events );
 
 			return $response;
 
@@ -45,13 +46,13 @@
 
 			$id = $request['id'];
 
-			$offer_query = "SELECT wp_posts.ID, post_title AS name, start_time, end_time, date, redeemable FROM wp_posts
-					INNER JOIN (" . $this->get_offer_meta() . ") AS times ON (wp_posts.ID = times.post_id)
-					WHERE post_type = 'offer' AND post_status = 'publish' AND ID = " . $id;
+			$event_query = "SELECT wp_posts.ID, post_title AS name, start_time, end_time, date, repeatable FROM wp_posts
+					INNER JOIN ( " . $this->get_event_meta() . " ) AS meta ON (wp_posts.ID = meta.post_id)
+					WHERE post_type = 'event' AND post_status = 'publish' AND ID = " . $id;
 
-			$offer = $wpdb->get_row($offer_query);
+			$event = $wpdb->get_row($event_query);
 
-			$response = rest_ensure_response($offer);
+			$response = rest_ensure_response($event);
 			return $response;
 
 		}
@@ -60,13 +61,13 @@
 
 			global $wpdb;
 
-			$offers_query = "SELECT wp_posts.ID, post_title AS name, start_time, end_time, date, redeemable FROM wp_posts
-					 INNER JOIN (" . $this->get_offer_meta() . ") AS times ON (wp_posts.ID = times.post_id)
-					 WHERE post_type = 'offer' AND post_status = 'publish'";
+			$events_query = "SELECT wp_posts.ID, post_title AS name, start_time, end_time, date, repeatable FROM wp_posts
+					 INNER JOIN ( " . $this->get_event_meta() . " ) AS meta ON (wp_posts.ID = meta.post_id)
+					 WHERE post_type = 'event' AND post_status = 'publish'";
 
-			$offers = $wpdb->get_results($offers_query);
+			$events = $wpdb->get_results($events_query);
 
-			foreach ($offers as $offer) {
+			foreach ($events as $event) {
 
 				/*$venue->address["address_line_1"] = $venue->address_line_1;
 				$venue->address["address_line_2"] = $venue->address_line_2;
@@ -94,7 +95,7 @@
 
 			}
 
-			$response  = rest_ensure_response( $offers );
+			$response  = rest_ensure_response( $events );
 
 			return $response;
 
@@ -102,13 +103,13 @@
 
 	}
 
-	function kino_register_offers_rest_routes() {
+	function kino_register_events_rest_routes() {
 
-		$controller = new Offers_Controller();
+		$controller = new Events_Controller();
 		$controller->register_routes();
 
 	}
 
-	add_action('rest_api_init', 'kino_register_offers_rest_routes');
+	add_action('rest_api_init', 'kino_register_events_rest_routes');
 
 ?>
