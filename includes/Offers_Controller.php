9e90931eacaca98bfdf5fde31d4d8e83daebf88a
James Lendrem <James@kinocreative.net>
2017-02-23 14:52:12 +0000

    Offers Controller now extends KCController

--- includes/Offers_Controller.php
+++ includes/Offers_Controller.php
@@ -1,46 +1,12 @@
 <?php
 
-	class Offers_Controller extends WP_REST_Posts_Controller {
+	require_once('KCController.php');
 
-		private $model;
+	class Offers_Controller extends KCController {
 
 		public function __construct() {
 
-			$this->model = new OfferModel();
-
-			parent::__construct('offer');
-
-		}
-
-		public function register_routes() {
-
-			parent::register_routes();
-
-		}
-
-		public function get_item($request) {
-
-			$id = $request['id'];
-			$offer = $this->model->getItem($id);
-
-			$response = rest_ensure_response($offer);
-			return $response;
-
-		}
-
-		public function get_items($request) {
-
-			$offers = $this->model->getItems();
-
-			foreach ($offers as $offer) {
-				
-				/** In case of clean up **/
-
-			}
-
-			$response  = rest_ensure_response( $offers );
-
-			return $response;
+			parent::__construct(new OfferModel(), 'offer');
 
 		}
 
