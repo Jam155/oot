3b4d5d03e223e41d5747419ac99531bbbda6d18f
James Lendrem <James@kinocreative.net>
2017-02-23 11:21:26 +0000

    Created OfferModel class and fleshed it out

---
+++ includes/models/OfferModel.php
@@ -0,0 +1,33 @@
+<?php
+
+	require_once("KCModel.php");
+
+	class OfferModel extends KCModel {
+
+		function __construct() {
+
+			$meta_columns = array(
+
+				"start_time" => "start_time",
+				"end_time" => "end_time",
+				"date" => "date",
+				"maximum_redeemable" => "redeemable",
+
+			);
+
+			$columns = array(
+
+				"wp_posts.ID" => "post_id",
+				"wp_posts.post_title" => "name",
+				"meta.start_time" => "start_time",
+				"meta.end_time" => "end_time",
+				"meta.date" => "date",
+				"meta.redeemable" => "redeemable",
+
+			);
+
+			$post_type = 'offer';
+
+			parent::__construct($meta_columns, $columns, $post_type);
+
+	}
